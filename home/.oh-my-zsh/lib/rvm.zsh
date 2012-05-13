86a919fa5be4feef7ba50e8594bd11aea1c6effe
Harry Garrood <hdgarrood@gmail.com>
2012-05-14 00:25:08 +0100

    Delete EVERYTHING

--- home/.oh-my-zsh/lib/rvm.zsh
+++
@@ -1,6 +0,0 @@
-## RVM
-# get the name of the branch we are on
-function rvm_prompt_info() {
-  ruby_version=$(~/.rvm/bin/rvm-prompt 2> /dev/null) || return
-  echo "($ruby_version)"
-}
--- home/.zshrc
+++
@@ -1,4 +0,0 @@
-ZSH=$HOME/.oh-my-zsh
-ZSH_THEME="prose"
-plugins=()
-source $ZSH/oh-my-zsh.sh
