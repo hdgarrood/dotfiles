498ca63871a63e2f0bd50f2af0378f1a18466bbe
Harry Garrood <hdgarrood@gmail.com>
2012-05-18 22:59:03 +0100

    add alias for ack

--- oh-my-zsh/lib/aliases.zsh
+++ oh-my-zsh/lib/aliases.zsh
@@ -1,7 +1,14 @@
 ## ALIASES
-alias history='fc -l 1 | less'
+
+# directories
 alias ls="ls --color=auto --hide='*.pyc'"
 alias lsa='ls -lAh'
 alias d='dirs -v'
+
+# shortcuts
 alias dj='cd ~/code/django/ && workon django-env'
 alias ra='cd ~/code/rails'
+
+# misc
+alias ack='ack-grep'
+alias history='fc -l 1 | less'
