93ed71f679f8c3e9196545962109386c8e34b591
Harry Garrood <hdgarrood@gmail.com>
2012-05-26 23:54:11 +0100

    remove mcd() -- never used it anyway

--- oh-my-zsh/lib/functions.zsh
+++ oh-my-zsh/lib/functions.zsh
@@ -3,10 +3,6 @@ function zsh_stats() {
   history | awk '{print $2}' | sort | uniq -c | sort -rn | head
 }
 
-function mcd() {
-  mkdir -p "$1" && cd "$1";
-}
-
 function extract () {
     if [ -f $1 ] ; then
         case $1 in
