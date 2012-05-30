8abcff718ee34a2ac44442fef776466a58657609
Harry Garrood <hdgarrood@gmail.com>
2012-05-30 22:38:59 +0100

    Update path to virtualenvwrapper

--- oh-my-zsh/lib/virtualenv.zsh
+++ oh-my-zsh/lib/virtualenv.zsh
@@ -2,6 +2,6 @@
 VIRTUAL_ENV_DISABLE_PROMPT="true"
 WORKON_HOME=$HOME/.virtualenv
 VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
-if [ -f /usr/bin/virtualenvwrapper.sh ]; then
-    source /usr/bin/virtualenvwrapper.sh
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+    source /usr/local/bin/virtualenvwrapper.sh
 fi
