67914b35dc5cfecdc1cfb6fd04f8d8e05b06bd36
Harry Garrood <hdgarrood@gmail.com>
2013-02-18 01:20:54 +0000

    replace tree.sh with a better version

--- bin/tree.sh
+++ bin/tree.sh
@@ -1,45 +1,58 @@
-#!/bin/sh
-#######################################################
-#  UNIX TREE                                          #
-#  File: ~/bin/tree.sh                                #
-#  created on : Mon Nov 28 08:19:08 IST 2011          #
-#  Displays Structure of Directory Hierarchy          #
-#  -------------------------------------------------  #
-#  This tiny script uses "ls", "grep", and "sed"      #
-#  in a single command to show the nesting of         #
-#  sub-directories.  The setup command for PATH       #
-#  works with the Bash shell (Ubuntu OS X default).   #
-#                                                     #
-#  Setup:                                             #
-#     $ cd ~/bin                                      #
-#     $ chmod u+x tree.sh                             #
-#     $ echo "PATH=~/bin:\${PATH}" >> ~/.profile      #
-#                                                     #
-#  Usage:                                             #
-#     $ tree.sh [directory]                           #
-#                                                     #
-#  Examples:                                          #
-#     $ tree.sh                                       #
-#     $ tree.sh /etc/opt                              #
-#     $ tree.sh ..                                    #
-#                                                     #
-#  Public Domain Software -- Free to Use as You Like  #
-#  http://www.nandal.in  -  By Sandeep nandal         #
-#######################################################
+#!/bin/bash
+# emulate the 'tree' program for listing all files and directories below the
+# current one.
 
-echo
-if [ "$1" != "" ]  #if parameter exists, use as base folder
-    then cd "$1"
-fi
-pwd
-ls -R | grep ":$" |   \
-    sed -e 's/:$//' -e 's/[^-][^\/]*\//--/g' -e 's/^/   /' -e 's/-/|/'
-# 1st sed: remove colons
-# 2nd sed: replace higher level folder names with dashes
-# 3rd sed: indent graph three spaces
-# 4th sed: replace first dash with a vertical bar
-if [ `ls -F -1 | grep "/" | wc -l` = 0 ]   # check if no folders
-    then echo "   -> no sub-directories"
-fi
-echo
-exit
+olddir=$PWD;
+color="off";
+stackheight=0;
+for option in "$@"
+do
+    case $option in
+        --color ) color="on"; shift;;
+        -*      ) shift;;
+        *       )      ;;
+    esac
+done
+
+function listfiles() {
+    cd "$1";
+    for file in *
+    do
+        for ((i=-1; $i < $stackheight; i++))
+        do
+            printf '  '
+        done
+
+        # indentation shows the depth of the file
+        if [ -d "$file" ]
+        then
+            if [ $color == "on" ]
+            then
+                printf "\e[34m";
+                # display directory names in blue
+            fi
+        fi
+
+        # print the filename
+        printf "$file"
+        if [ $color == "on" ]; then printf "\e[0m"; fi
+        printf "\n"
+
+        # recurse if we're in a directory
+        if [ -d "$file" ]
+        then
+            stackheight=$stackheight+1;
+            # more indentation
+            listfiles "$file";
+            # recursive listing of files
+            cd ..;
+        fi
+    done
+    let stackheight=$stackheight-1;
+    # less indentation
+}
+
+echo `pwd`:
+listfiles "$1";
+cd $olddir;
+unset i color stackheight;
