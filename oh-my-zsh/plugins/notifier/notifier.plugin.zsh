ba2160b5fcb90011e1a88ae513f75b855d480978
Harry Garrood <h@hdgarrood.me.uk>
2013-04-05 23:15:48 +0100

    more sophisticated time reporting

--- oh-my-zsh/plugins/notifier/notifier.plugin.zsh
+++ oh-my-zsh/plugins/notifier/notifier.plugin.zsh
@@ -1,3 +1,35 @@
+seconds-to-hms() {
+    echo $1 | awk '
+        function join(array, start, end, sep, result, i)
+        {
+            if (sep == "")
+                sep = " "
+            else if (sep == SUBSEP) # magic value
+                sep = ""
+            result = array[start]
+            for (i = start + 1; i <= end; i++)
+                result = result sep array[i]
+            return result
+        }
+
+        function hms(s)
+        {
+            h = int(s/3600)
+            s = s-(h*3600)
+            m = int(s/60)
+            s = s-(m*60)
+
+            i = 0
+
+            if (h > 0) { result[i] = sprintf("%d hours",   h); i++; }
+            if (m > 0) { result[i] = sprintf("%d minutes", m); i++; }
+            if (s > 0) { result[i] = sprintf("%d seconds", s); i++; }
+
+            print(join(result, 0, i-1, ", "));
+        }
+        { hms($0); }'
+}
+
 if [[ -x `which notify-send` ]]; then
     notify-preexec-hook() {
         zsh_notifier_cmd="$1"
@@ -10,8 +42,8 @@ if [[ -x `which notify-send` ]]; then
         if [[ "${zsh_notifier_cmd}" != "" ]]; then
             time_taken=$(( `date +%s` - ${zsh_notifier_time} ))
             if (( $time_taken > $REPORTTIME )); then
-                notify-send "task finished" \
-                    "'$zsh_notifier_cmd' exited with status $? after $time_taken seconds"
+                msg="'$zsh_notifier_cmd' exited with status $? after `seconds-to-hms $time_taken`"
+                notify-send "task finished" "$msg"
             fi
         fi
         zsh_notifier_cmd=
