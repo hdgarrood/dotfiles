c3eb2df847c03dd5978c404d07ffb91b0b4fcb53
Harry Garrood <hdgarrood@gmail.com>
2013-05-17 17:23:26 +0100

    add powershell profile

---
+++ _extras/profile.ps1
@@ -0,0 +1,25 @@
+function to-array {
+  begin { $output = @(); }
+  process { $output += $_; }
+  end { return ,$output; }
+}
+
+function which($cmd) {
+    get-command $cmd | select path
+}
+
+# posh-git stuff
+import-module posh-git 2>&1 | out-null
+if ($?) {
+    function prompt {
+        $tmp_exit_code = $?
+        $Host.UI.RawUI.ForegroundColor = $GitPromptSettings.DefaultForegroundColor
+        write-host "PS: " -nonewline
+        write-host (get-location) -nonewline
+        write-vcsstatus
+        write-host "`n" -nonewline
+        return "> "
+    }
+
+    enable-gitcolors
+}
