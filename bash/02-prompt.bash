7d9757ccd018a33f51f3b1f3df216c0b49abe7d6
Harry Garrood <harry@garrood.me>
2014-02-10 23:49:56 +0000

    git bash prompt: Enable extra info

--- bash/02-prompt.bash
+++ bash/02-prompt.bash
@@ -1,3 +1,9 @@
+# Git prompt info settings
+GIT_PS1_SHOWDIRTYSTATE=true
+GIT_PS1_SHOWSTASHSTATE=true
+GIT_PS1_SHOWUNTRACKEDFILES=true
+GIT_PS1_SHOWUPSTREAM=auto
+
 __bash_prompt() {
     local RESETCOLOUR="\033[0m"
     local CYAN="\033[0;36m"
