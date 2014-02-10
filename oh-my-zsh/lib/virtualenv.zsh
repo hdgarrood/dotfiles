990756d376d88d0b62d7eea24544e8a7ac8bccee
Harry Garrood <harry@garrood.me>
2014-02-10 21:20:09 +0000

    Remove oh-my-zsh

--- oh-my-zsh/lib/virtualenv.zsh
+++
@@ -1,7 +0,0 @@
-## VIRTUALENV
-VIRTUAL_ENV_DISABLE_PROMPT="true"
-WORKON_HOME=$HOME/.virtualenv
-VIRTUALENVWRAPPER_PYTHON=/usr/bin/python2.7
-if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
-    source /usr/local/bin/virtualenvwrapper.sh
-fi
--- zshrc
+++
@@ -1,3 +0,0 @@
-ZSH=$HOME/.oh-my-zsh
-plugins=("rbenv" "notifier")
-source $ZSH/oh-my-zsh.sh
