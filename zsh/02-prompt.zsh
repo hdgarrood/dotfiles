8191a2a3a57e92b7e5640694f4bf5f93c523dc12
Harry Garrood <harry@garrood.me>
2026-04-26 01:34:58 +0100

    Fix jj prompt info

--- zsh/02-prompt.zsh
+++ zsh/02-prompt.zsh
@@ -1,5 +1,5 @@
 # Allow command substitution
 setopt prompt_subst
 
-PS1="%F{cyan}%n%f@%F{yellow}%m%f: %F{green}%~%f %F{blue}\$(nix-shell-info)%f %F{5}\$(jj-prompt-info)%f
+PS1="%F{cyan}%n%f@%F{yellow}%m%f: %F{green}%~%f %F{blue}\$(nix-shell-info)%f %F{5}\$(jj-prompt-info \"(%s)\")%f
 $ "
