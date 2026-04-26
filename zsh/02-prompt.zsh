c62b451beb7b11fb7e308f9d157c4e044e561c9c
Harry Garrood <harry@garrood.me>
2026-04-26 01:28:01 +0100

    Switch git => jj prompt info

--- zsh/02-prompt.zsh
+++ zsh/02-prompt.zsh
@@ -1,11 +1,5 @@
-# Git prompt info settings
-GIT_PS1_SHOWDIRTYSTATE=true
-GIT_PS1_SHOWSTASHSTATE=true
-GIT_PS1_SHOWUNTRACKEDFILES=true
-GIT_PS1_SHOWUPSTREAM=auto
-
 # Allow command substitution
 setopt prompt_subst
 
-PS1="%F{cyan}%n%f@%F{yellow}%m%f: %F{green}%~%f %F{blue}\$(nix-shell-info)%f %F{5}\$(__git_ps1 \"(%s)\")%f
+PS1="%F{cyan}%n%f@%F{yellow}%m%f: %F{green}%~%f %F{blue}\$(nix-shell-info)%f %F{5}\$(jj-prompt-info)%f
 $ "
