d997f62ce5aa310998d9f31013a2abfcec4054db
Harry Garrood <harry@garrood.me>
2026-04-26 13:37:50 +0100

    Show previous exit code in prompt

--- zsh/02-prompt.zsh
+++ zsh/02-prompt.zsh
@@ -1,5 +1,6 @@
 # Allow command substitution
 setopt prompt_subst
 
-PS1="%F{cyan}%n%f@%F{yellow}%m%f: %F{green}%~%f %F{blue}\$(nix-shell-info)%f %F{5}\$(jj-prompt-info \"(%s)\")%f
+PS1="%(?..%B%F{red}[%?]%f%b
+)%F{cyan}%n%f@%F{yellow}%m%f: %F{green}%~%f %F{blue}\$(nix-shell-info)%f %F{5}\$(jj-prompt-info \"(%s)\")%f
 $ "
