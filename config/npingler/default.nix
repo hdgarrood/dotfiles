ed79828ea715458305d293e54cd8009392384d7c
Harry Garrood <harry@garrood.me>
2026-05-10 18:57:30 +0100

    npingler: init

---
+++ config/npingler/default.nix
@@ -0,0 +1,43 @@
+let
+  npins-sources = import ./npins;
+  pkgs = import npins-sources.nixpkgs {
+    overlays = [
+      (final: prev: {
+        inherit npins-sources;
+
+        npingler-lib = final.callPackage "${npins-sources.npingler}/lib" { };
+      })
+    ];
+  };
+  profile = pkgs.npingler-lib.makeProfile {
+    pins = {
+      # A map of names to `source` derivations. These get pinned in the `nix
+      # registry` so that (e.g.) `nix repl nixpkgs` uses the same version of
+      # `nixpkgs` as your profile, and also in your Nix channels, so that
+      # `nix-shell -p hello` uses the same version as well.
+      nixpkgs = npins-sources.nixpkgs;
+    };
+
+    paths = [
+      pkgs.direnv
+      pkgs.fish
+      pkgs.fzf
+      pkgs.gh
+      pkgs.git
+      pkgs.jq
+      pkgs.jujutsu
+      pkgs.neovim
+      pkgs.nix-direnv
+      pkgs.nix-your-shell
+      pkgs.ripgrep
+      pkgs.watchman
+    ];
+  };
+in
+{
+  npingler = {
+    # By default, npingler uses the attr matching your hostname.
+    sunbird = profile;
+  };
+}
+
