251578fcb291bfbc5ce89d7f45de395e7872570a
Harry Garrood <harry@garrood.me>
2026-05-15 14:23:43 +0100

    fish: source nix-provided fish config

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -13,16 +13,24 @@ end
 if status --is-interactive
   fish_vi_key_bindings
 
-  if command -v buck2 >/dev/null
-    buck2 completion fish | source
+  # Nix-provided fish config is added to XDG_DATA_DIRS too late, after fish has
+  # already sourced vendor config. This fixes that.
+  #
+  # We need this for the direnv hook and for completions for nix-provided
+  # programs in particular.
+  set --global --append fish_complete_path \
+      ~/.nix-profile/share/fish/vendor_completions.d \
+      ~/.nix-profile/share/fish/completions
+  set --global --append fish_function_path \
+      ~/.nix-profile/share/fish/vendor_functions.d \
+      ~/.nix-profile/share/fish/functions
+  # https://github.com/fish-shell/fish-shell/issues/10078#issuecomment-1786490675
+  for file in ~/.nix-profile/share/fish/vendor_conf.d/*.fish
+      source $file
   end
 
   if command -v nix-your-shell >/dev/null
     nix-your-shell fish | source
-  end
-
-  if command -v direnv >/dev/null
-    direnv hook fish | source
   end
 end
 
