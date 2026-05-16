d1f354491c6604d4df2746990ff5ff1c4acf86e9
Harry Garrood <harry@garrood.me>
2026-05-16 01:03:08 +0100

    remove zsh config

--- zsh/02-prompt.zsh
+++
@@ -1,6 +0,0 @@
-# Allow command substitution
-setopt prompt_subst
-
-PS1="%(?..%B%F{red}[%?]%f%b
-)%F{cyan}%n%f@%F{yellow}%m%f: %F{green}%~%f %F{blue}\$(nix-shell-info)%f %F{5}\$(jj-prompt-info \"(%s)\")%f
-$ "
--- zsh/03-shell-completion.zsh
+++
@@ -1,5 +0,0 @@
-autoload -Uz compinit
-compinit
-
-command -v jj >/dev/null && source <(COMPLETE=zsh jj)
-command -v buck2 >/dev/null && source <(buck2 completion zsh)
