8e3afd43100b24b0004fa7f2fb09b69d6a4dc1dd
Harry Garrood <harry@garrood.me>
2026-05-16 16:11:41 +0100

    npingler: refactor

--- config/npingler/default.nix
+++ config/npingler/default.nix
@@ -5,57 +5,71 @@ let
     overlays = [
       (final: prev: {
         npingler-lib = final.callPackage "${sources.npingler}/lib" { };
-      })
-      (final: prev: {
         hdgarrood = final.callPackage ./makePkgs.nix { };
       })
     ];
   };
 
+  # A map of names to `source` derivations. These get pinned in the `nix
+  # registry` so that (e.g.) `nix repl nixpkgs` uses the same version of
+  # `nixpkgs` as your profile, and also in your Nix channels, so that
+  # `nix-shell -p hello` uses the same version as well.
+  pins = {
+    nixpkgs = sources.nixpkgs;
+  };
+
+  # Packages to be added to your profile.
+  paths = [
+    pkgs.buck2 # for completions, mainly.
+    pkgs.coreutils
+    pkgs.eza
+    pkgs.direnv
+    pkgs.fd
+    pkgs.fish
+    pkgs.fnm
+    pkgs.fzf
+    pkgs.gh
+    pkgs.git
+    pkgs.graphviz
+    pkgs.jq
+    pkgs.jujutsu
+    pkgs.neovim
+    pkgs.nix-your-shell
+    pkgs.nixfmt
+    pkgs.npingler
+    pkgs.npins
+    pkgs.numbat
+    pkgs.ripgrep
+    pkgs.ruby
+    pkgs.terminal-notifier
+    pkgs.watchman
+
+    pkgs.hdgarrood.nix-direnv # for the "print at most 5 changed files" patch only
+    pkgs.hdgarrood.jj-prompt-info
+    pkgs.hdgarrood.nix-prompt-info
+  ];
+
   profile = pkgs.npingler-lib.makeProfile {
-    pins = {
-      # A map of names to `source` derivations. These get pinned in the `nix
-      # registry` so that (e.g.) `nix repl nixpkgs` uses the same version of
-      # `nixpkgs` as your profile, and also in your Nix channels, so that
-      # `nix-shell -p hello` uses the same version as well.
-      nixpkgs = sources.nixpkgs;
-    };
-
-    paths = [
-      pkgs.buck2 # for completions, mainly.
-      pkgs.coreutils
-      pkgs.eza
-      pkgs.direnv
-      pkgs.fd
-      pkgs.fish
-      pkgs.fnm
-      pkgs.fzf
-      pkgs.gh
-      pkgs.git
-      pkgs.graphviz
-      pkgs.jq
-      pkgs.jujutsu
-      pkgs.neovim
-      pkgs.nix-your-shell
-      pkgs.nixfmt
-      pkgs.npingler
-      pkgs.npins
-      pkgs.numbat
-      pkgs.ripgrep
-      pkgs.ruby
-      pkgs.terminal-notifier
-      pkgs.watchman
-
-      pkgs.hdgarrood.nix-direnv # for the "print at most 5 changed files" patch only
-      pkgs.hdgarrood.jj-prompt-info
-      pkgs.hdgarrood.nix-prompt-info
-    ];
+    inherit paths pins;
   };
 in
 {
-  debug = {
-    inherit sources pkgs;
-  };
+  inherit
+    sources
+    pkgs
+    pins
+    paths
+    ;
+
+  summary = pkgs.lib.listToAttrs (
+    builtins.map (p: {
+      name = p.pname or p.name;
+      value = {
+        version = builtins.toString p.version or "0";
+        storePath = builtins.storePath p;
+      };
+    }) paths
+  );
 
   npingler = {
     # By default, npingler uses the attr matching your hostname.
@@ -63,4 +77,3 @@ in
     rook = profile;
   };
 }
-
